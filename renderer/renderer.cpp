3daea9951c4579373b611f2cb33214d40bc7d491
한솔 <hansol@hansol-ui-MacBookAir.local>
2022-02-27 21:40:11 +0900

    first branch called develop

--- renderer/renderer.cpp
+++ renderer/renderer.cpp
@@ -16,7 +16,18 @@ void renderer::initializeInstance(){
 			throw std::runtime_error("Validation layers were requested, but are not available");
 		}
 	}
-		
+	
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "my universe";
+	appInfo.applicationVersion = VK_MAKE_VERSION(0,0,1);
+	appInfo.pEngineName = "my universe engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(0,0,1);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
 }
 void renderer::initializeSurface(){
 }
